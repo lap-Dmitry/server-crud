8c265b38439bbb142e98953b310cd69ac7a9e15e
Dmitry Lapshin <76706628+lap-Dmitry@users.noreply.github.com>
2022-01-14 21:41:30 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = new Koa();
 app.use(cors());
 app.use(koaBody({json: true}));
 
-const notes = [];
+const notes = [{id: 0, content: 'Hello world!'},];
 let nextId = 1;
 
 const router = new Router();
